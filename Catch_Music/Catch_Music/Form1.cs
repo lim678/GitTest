1979f7c9dd986ff002779c65eb44e87b4e19efc0
lim678 <kyounglimb0@gmail.com>
2023-05-11 14:02:28 +0900

    test2

--- Catch_Music/Catch_Music/Form1.cs
+++ Catch_Music/Catch_Music/Form1.cs
@@ -16,5 +16,10 @@ namespace Catch_Music
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
     }
 }
